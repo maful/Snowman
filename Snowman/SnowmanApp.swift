bef59e94e39b3dcad7c1c29d692d71cd1edeac4e
maful <me@maful.web.id>
2023-06-07 21:52:36 +0700

    Initial Commit

---
+++ Snowman/SnowmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowmanApp.swift
+//  Snowman
+//
+//  Created by Maful on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
